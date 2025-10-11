2f8156128107d326da57815da5e43c005f5a890c
zaridias <zaridias@outlook.com>
2025-10-11 05:01:27 +0000

    params

--- main.tf
+++ main.tf
@@ -16,5 +16,7 @@ data "cloudflare_account" "zaridias" {
 resource "cloudflare_zero_trust_organization" "amg45" {
   name = "amg45coupe"
   is_ui_read_only = true
+  auto_redirect_to_identity = true
+  allow_authenticate_via_warp = true
   account_id = data.cloudflare_account.zaridias.account_id
 }
